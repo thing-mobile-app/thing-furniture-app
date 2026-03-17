f3be3167b0a10817fedfa72f6ea02c1cfb8dd30a
Doğukan Süme <dogubey2112@gmail.com>
2026-03-17 20:54:06 +0300

    feat: add INTRODUCTION_KEY to Constants

--- app/src/main/java/com/example/thingapp/util/Constants.kt
+++ app/src/main/java/com/example/thingapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val USER_COLLECTION = "user"
     // Shared Preferences name for introduction flow
     const val INTRODUCTION_SP = "IntroductionSP"
+    // Key for the introduction flow status
+    const val INTRODUCTION_KEY = "IntroductionKey"
 }
