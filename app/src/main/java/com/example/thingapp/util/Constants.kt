70f56d4a9cc68916d76a46026735acc93d3e6ed6
Doğukan Süme <dogubey2112@gmail.com>
2026-03-17 19:40:48 +0300

    feat: add Introduction Shared Preferences to Constants and AppModule

--- app/src/main/java/com/example/thingapp/util/Constants.kt
+++ app/src/main/java/com/example/thingapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.thingapp.util
 
 object Constants {
     const val USER_COLLECTION = "user"
-
-
+    // Shared Preferences name for introduction flow
+    const val INTRODUCTION_SP = "IntroductionSP"
 }
