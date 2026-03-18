6c55532b17cd3982627a6803ce00e9db8347b346
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2026-03-18 11:06:35 +0300

    Smart Intro Navigation (#2)
    * feat: add Introduction Shared Preferences to Constants and AppModule
    
    * feat: add IntroductionViewModel and handle navigation logic
    
    * feat: add INTRODUCTION_KEY to Constants
    
    * feat: setup navigation observation and button click in IntroductionFragment

--- app/src/main/java/com/example/thingapp/util/Constants.kt
+++ app/src/main/java/com/example/thingapp/util/Constants.kt
@@ -2,6 +2,8 @@ package com.example.thingapp.util
 
 object Constants {
     const val USER_COLLECTION = "user"
-
-
+    // Shared Preferences name for introduction flow
+    const val INTRODUCTION_SP = "IntroductionSP"
+    // Key for the introduction flow status
+    const val INTRODUCTION_KEY = "IntroductionKey"
 }
