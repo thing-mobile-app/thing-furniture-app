61e28cc9b4c1ed6175e527938d4d344b65a87c79
Kağan Şahin <kaganxsahin@gmail.com>
2026-04-01 01:49:31 +0300

    feat: implement special products retrieval and display in MainCategory

--- app/src/main/java/com/example/thingapp/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/thingapp/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,67 @@
 package com.example.thingapp.fragments.categories
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.thingapp.R
+import com.example.thingapp.adapters.SpecialProductsAdapter
+import com.example.thingapp.databinding.FragmentMainCategoryBinding
+import com.example.thingapp.util.Resource
+import com.example.thingapp.viewmodel.MainCategoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
-class MainCategoryFragment : Fragment(R.layout.fragment_main_category){
+@AndroidEntryPoint
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+    private lateinit var binding: FragmentMainCategoryBinding
+    private val specialProductsAdapter by lazy { SpecialProductsAdapter() }
+    private val viewModel by viewModels<MainCategoryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentMainCategoryBinding.bind(view)
+
+        setupSpecialProductsRv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.specialProducts.collectLatest { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        showLoading()
+                    }
+                    is Resource.Success -> {
+                        specialProductsAdapter.differ.submitList(resource.data)
+                        hideLoading()
+                    }
+                    is Resource.Error -> {
+                        hideLoading()
+                        Log.e("MainCategoryFragment", resource.message.toString())
+                        Toast.makeText(requireContext(), resource.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private fun setupSpecialProductsRv() {
+        binding.rvSpecialProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = specialProductsAdapter
+        }
+    }
+
+    private fun showLoading() {
+        binding.mainCategoryProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        binding.mainCategoryProgressBar.visibility = View.GONE
+    }
 }
