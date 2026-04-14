9e0c718c0e4f86f9a1bea3f067b0db72359e3d88
Doğukan Süme <dogubey2112@gmail.com>
2026-04-14 03:38:01 +0300

    feat: add product information screen.

--- app/src/main/java/com/example/thingapp/fragments/shopping/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/thingapp/fragments/shopping/ProductDetailsFragment.kt
@@ -1,4 +1,83 @@
 package com.example.thingapp.fragments.shopping
 
-class ProductDetailsFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.thingapp.adapters.ColorsAdapter
+import com.example.thingapp.adapters.SizesAdapter
+import com.example.thingapp.adapters.ViewPager2Images
+import com.example.thingapp.databinding.FragmentProductDetailsBinding
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.fragment.findNavController
+import com.example.thingapp.util.hideBottomNavigationView
+
+class ProductDetailsFragment: Fragment() {
+    private val args by navArgs<ProductDetailsFragmentArgs>()
+    private lateinit var binding: FragmentProductDetailsBinding
+    private val viewPagerAdapter by lazy { ViewPager2Images() }
+    private val sizesAdapter by lazy { SizesAdapter() }
+    private val colorsAdapter by lazy { ColorsAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProductDetailsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        hideBottomNavigationView()
+
+        val product = args.product
+
+        setupSizesRv()
+        setupColorsRv()
+        setupViewpager()
+
+        binding.apply {
+            tvProductName.text = product.name
+            tvProductPrice.text = "$ ${product.price}"
+            tvProductDescription.text = product.description
+
+            if (product.colors.isNullOrEmpty())
+                tvProductColors.visibility = View.INVISIBLE
+            if (product.sizes.isNullOrEmpty())
+                tvProductSize.visibility = View.INVISIBLE
+
+            imageClose.setOnClickListener {
+                findNavController().navigateUp()
+            }
+        }
+
+        viewPagerAdapter.differ.submitList(product.images)
+        product.colors?.let { colorsAdapter.differ.submitList(it) }
+        product.sizes?.let { sizesAdapter.differ.submitList(it) }
+    }
+
+    private fun setupViewpager() {
+        binding.apply {
+            viewPagerProductImages.adapter = viewPagerAdapter
+        }
+    }
+
+    private fun setupColorsRv() {
+        binding.rvColors.apply {
+            adapter = colorsAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    private fun setupSizesRv() {
+        binding.rvSize.apply {
+            adapter = sizesAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
 }
