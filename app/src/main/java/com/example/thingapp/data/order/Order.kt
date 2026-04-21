529b0872231b71041b9a0064a0e2b8631b6da0ca
Doğukan Süme <dogubey2112@gmail.com>
2026-04-21 21:34:12 +0300

    feat: add orders screen.

--- app/src/main/java/com/example/thingapp/data/order/Order.kt
+++ app/src/main/java/com/example/thingapp/data/order/Order.kt
@@ -2,16 +2,18 @@ package com.example.thingapp.data.order
 
 import com.example.thingapp.data.Address
 import com.example.thingapp.data.CartProduct
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.random.Random.Default.nextLong
 
 /**
  * Data class representing a completed purchase order.
- *
- * Includes the current status, total cost, list of purchased products,
- * and the designated shipping address.
  */
 data class Order(
     val orderStatus: String = "",
     val totalPrice: Float = 0f,
     val products: List<CartProduct> = emptyList(),
-    val address: Address = Address()
+    val address: Address = Address(),
+    val date: String = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(Date()),
+    val orderId: Long = nextLong(0, 100_000_000_000) + totalPrice.toLong()
 )
